01537130a163802c25b84c0dc1fbbc486bf7c13f
Chetan Malabade <malabade.chetan@gmail.com>
2024-06-16 00:55:48 +0530

    added student class entity

--- src/main/java/java/alien/App.java
+++ src/main/java/java/alien/App.java
@@ -11,5 +11,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        Student s=new  Student();
+        s.setId(101);
+        s.setName("javaalien");
+        
+        System.out.println(s.getId() +" - " +s.getName());
     }
 }
