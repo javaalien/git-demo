b2f5e0732a3dc29e5919ef76011ecf2c49315683
Chetan Malabade <malabade.chetan@gmail.com>
2024-06-16 00:05:04 +0530

    added if condition to check the data

--- src/main/java/java/alien/App.java
+++ src/main/java/java/alien/App.java
@@ -8,6 +8,9 @@ public class App
 {
 	public void addData(String data) {
 		System.out.println("Data = "+data);
+		if(data.equals("java alien")) {
+			System.out.println("Welcome to git");
+		}
 	}
 	
     public static void main( String[] args )
